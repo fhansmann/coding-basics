c14feb245650e3089ab063c64edbc02eb984e202
Florian Hansmann <30904740+fhansmann@users.noreply.github.com>
2020-01-31 18:46:54 +0100

    Update tuple-to-dict.py

--- mini-programs/tuple-to-dict.py
+++ mini-programs/tuple-to-dict.py
@@ -5,7 +5,6 @@ def Convert(tup, di):
         di.setdefault(a, []).append(b) 
     return di 
       
- 
 tups = [("akash", 10), ("gaurav", 12), ("anand", 14),  
      ("suraj", 20), ("akhil", 25), ("ashish", 30)] 
      
