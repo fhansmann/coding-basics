2b47adaccccb69553ebd4a938abc5d6f741336e5
Florian Hansmann <30904740+fhansmann@users.noreply.github.com>
2020-02-11 19:43:05 +0100

    Update simple-decorator.py

--- mini-programs/simple-decorator.py
+++ mini-programs/simple-decorator.py
@@ -27,3 +27,17 @@ Before calling foo
 Hi, foo has been called with 42
 After calling foo
 """
+# Decorator Syntax:
+
+def our_decorator(func):
+    def function_wrapper(x):
+        print("Before calling " + func.__name__)
+        func(x)
+        print("After calling " + func.__name__)
+    return function_wrapper
+
+@our_decorator
+def foo(x):
+    print("Hi, foo has been called with " + str(x))
+
+foo("Hi")
